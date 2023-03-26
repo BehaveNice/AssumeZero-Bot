fa18e20429d8cc498f6b81066a7ff04ece68e481
marqueis <marqueis@sheridancollege.ca>
2023-03-26 19:51:40 -0400

    Update config.js
    New bot config

--- src/config.js
+++ src/config.js
@@ -14,9 +14,9 @@ exports.owner = {
 exports.bot = {
     "names": {
         "short": "Alhpa Whore", // Will be used as 'nickname' in chat (remove if same as account name)
-        "long": "Alfajor Bot" // Name displayed in profile on bot's Facebook account
+        "long": "Elizabeth Wilson" // Name displayed in profile on bot's Facebook account
     },
-    "id": "100041587845629" // Bot's ID (for security purposes in commands)
+    "id": "100091143975670" // Bot's ID (for security purposes in commands)
 };
 
 // Location of the bot repo if automatic GitHub webhook deploys are used
@@ -59,7 +59,7 @@ exports.order66Time = 15;
 exports.banTime = 30;
 
 // Number of times to message user with the "wake up" command
-exports.wakeUpTimes = 20;
+exports.wakeUpTimes = 25;
 
 // Default color (hex)
 exports.defaultColor = "#67B868";
